9973d024703ae355c15b8dd7c8a8a7fb316ad093
Kaushik Das <kudokaito.pd@gmail.com>
2025-11-10 20:28:26 +0530

    Implement minOperations method in Solution class

--- HashMap/3542. Minimum Operations to Convert All Elements to Zero/Solution.java
+++ HashMap/3542. Minimum Operations to Convert All Elements to Zero/Solution.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int minOperations(int[] nums) {
+        int ops=0;
+        Deque<Integer> stack=new ArrayDeque<>();
+        for(int x:nums){
+            if(x==0){
+                stack.clear();
+                continue;
+            }
+            while(!stack.isEmpty() && stack.peekLast()>x){
+                stack.pollLast();
+            }
+            if(stack.isEmpty() || stack.peekLast()<x){
+                stack.addLast(x);
+                ops++;
+            }
+        }
+        return ops;
+    }
+}
