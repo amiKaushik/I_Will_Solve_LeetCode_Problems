87815b0e2414e69305d2909c1e56bbd9bd16bad3
Kaushik Das <kudokaito.pd@gmail.com>
2025-11-10 20:39:26 +0530

    Implement lengthOfLongestSubstring method

--- HashMap/3. Longest Substring Without Repeating Characters/Solution.java
+++ HashMap/3. Longest Substring Without Repeating Characters/Solution.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set=new HashSet<>();
+        int left=0,right=0,maxLen=0;
+        while(right<s.length()){
+            char ch=s.charAt(right);
+            if(!set.contains(ch)){
+                set.add(ch);
+                maxLen=Math.max(maxLen,right-left+1);
+                right++;
+            }else{
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return maxLen;
+    }
+}
