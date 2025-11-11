eecff48b667036b6caddce5f41321e020e42578e
Kaushik Das <kudokaito.pd@gmail.com>
2025-11-11 21:25:21 +0530

    Fix variable comment for twoStepBefore in climbStairs

--- Dynamic_Programming/70. Climbing Stairs/Solution.java
+++ Dynamic_Programming/70. Climbing Stairs/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int climbStairs(int n) {
         if(n<=2) return n;
         int oneStepBefore=2; // dp[i-1]
-        int twoStepBefore=1; // dp[i-1]
+        int twoStepBefore=1; // dp[i-2]
         int curr=0;
         for(int i=3;i<=n;i++){
             curr = oneStepBefore + twoStepBefore;
