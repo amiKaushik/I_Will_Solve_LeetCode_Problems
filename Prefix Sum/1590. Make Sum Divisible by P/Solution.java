eb7d48661f706e129312914ea79c3e79f7d7ea12
Kaushik Das <kudokaito.pd@gmail.com>
2025-11-30 12:49:27 +0530

    Implement minSubarray method for sum divisibility

--- Prefix Sum/1590. Make Sum Divisible by P/Solution.java
+++ Prefix Sum/1590. Make Sum Divisible by P/Solution.java
@@ -1,1 +1,32 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int n=nums.length;
+        long total=0L;
+        for(int num:nums) total += num;
+        int target = (int)(total%p);
+        if(target==0) return 0;
 
+        Map<Integer, Integer> position = new HashMap<>();
+        position.put(0,-1);
+        int ans=n;
+        long currSum=0L;
+
+        for(int i=0;i<n;i++){
+            currSum += nums[i];
+            int curr=(int)(currSum%p);
+            int need = curr-target;
+            if(need<0) need +=p;
+
+            Integer prevIndex = position.get(need);
+            if(prevIndex != null){
+                ans=Math.min(ans, i-prevIndex);
+            }
+            position.put(curr, i);
+        }
+        if(ans==n){
+            return -1;
+        }else{
+            return ans;
+        }
+    }
+}
