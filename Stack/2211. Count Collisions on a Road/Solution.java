695dfda7174cff920c5e71d98f68da4e1f55afbb
Kaushik Das <kudokaito.pd@gmail.com>
2025-12-04 21:07:52 +0530

    Update Solution.java

--- Stack/2211. Count Collisions on a Road/Solution.java
+++ Stack/2211. Count Collisions on a Road/Solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n=directions.length();
+        int left=0, right=n-1;
 
+        while(left<n && directions.charAt(left)=='L'){
+            left++;
+        }
+        while(right>=0 && directions.charAt(right)=='R'){
+            right--;
+        }
+        if(left>right) return 0;
+        int collisions=0;
+        for(int i=left;i<=right;i++){
+            if(directions.charAt(i)!='S'){
+                collisions++;
+            }
+        }
+        return collisions;
+    }
+}
