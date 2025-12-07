98743b128e192d8791565a5b240f2485fa4b40ef
Kaushik Das <kudokaito.pd@gmail.com>
2025-12-07 21:25:46 +0530

    Update Solution.java

--- Math/1523. Count Odd Numbers in an Interval Range/Solution.java
+++ Math/1523. Count Odd Numbers in an Interval Range/Solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int countOdds(int low, int high) {
+        int L=high-low+1;
+        if(L%2 == 0){
+            return L/2;
+        }
+        if(low%2==1){
+            return L/2 + 1;
+        }else{
+            return L/2;
+        }
+    }
+}
