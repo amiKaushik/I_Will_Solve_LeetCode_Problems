f20f4dac21565382e5b6c655cc7b2f3e9e4d294b
Kaushik Das <kudokaito.pd@gmail.com>
2025-12-08 23:59:14 +0530

    Update Solution.java

--- Math/1925. Count Square Sum Triples/Solution.java
+++ Math/1925. Count Square Sum Triples/Solution.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int countTriples(int n) {
+        int ans=0;
+        for(int c=1;c<=n;c++){
+            int a=1,b=c-1,c2=c*c;
+            while(a<b){
+                int s = a*a + b*b;
+                if(s==c2){
+                    ans+=2;
+                    a++;
+                    b--;
+                }else if(s<c2){
+                    a++;
+                }else{
+                    b--;
+                }
+            }
+        }
+        return ans;
+    }
+}
