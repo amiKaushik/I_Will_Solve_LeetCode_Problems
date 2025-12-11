cb89c5a5d5db28ecebf06fc5f01e7671e8d8d195
Kaushik Das <kudokaito.pd@gmail.com>
2025-12-11 20:15:58 +0530

    Update Solution.java

--- Array/3577. Count the Number of Computer Unlocking Permutations/Solution.java
+++ Array/3577. Count the Number of Computer Unlocking Permutations/Solution.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    private static final long MOD = 1_000_000_007L;
+    public int countPermutations(int[] complexity) {
+        int n=complexity.length;
+        long minVal=Long.MAX_VALUE;
+        int cntMin=0;
+        for(int v:complexity){
+            if(v<minVal){
+                minVal=v;
+                cntMin=1;
+            }else if(v==minVal){
+                cntMin++;
+            }
+        }
+        if(complexity[0] != minVal || cntMin>1){
+            return 0;
+        }
+        long ans=1L;
+        for(int i=2;i<=n-1;i++){
+            ans=(ans*i)%MOD;
+        }
+        return (int)ans;
+    }
+}
